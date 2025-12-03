4724a047b030d63bed9591943ae12abc274fa291
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-12-03 09:37:49 +0100

    refactor into two separate functions for data collection and plotting

--- plotting/plot.py
+++ plotting/plot.py
@@ -31,6 +31,45 @@ def get_environments(fp_orb_basedir:str, settings) -> [str]:
 
     return environments
 
+def getdata_recovery(fp_orb_basedir:str, settings) -> pd.DataFrame:
+    """Collects data from Orb for contig recovery analysis.
+    
+    Parameters
+    ----------
+    fp_orb_basedir : str
+        The filepath to Orb's base data dir
+    settings : yaml-dict
+        General settings for plotting Orb graphs. Here, we need
+        a) the environment to skip
+
+    Returns
+    -------
+    A pandas.DataFrame that holds performance parameters for assemblers in the different environments.
+    """
+    environments = get_environments(fp_orb_basedir, settings)
+
+    data = []
+    for i, environment in enumerate(environments):
+        # load orb data
+        orb = pd.read_csv(join(fp_orb_basedir, environment, 'mergedresults', '%s_all_scores.tsv' % environment), sep="\t", index_col=0)
+        # rename metrics to names used in publication
+        orb = orb.rename(index={k: c['label'] for k, c in settings['contig_classes'].items()})
+        # add information about missed blocks
+        trueBlocks = pd.read_csv(join(fp_orb_basedir, environment, 'mergedataframessummaries', '%s_all_contigs.tsv' % environment), sep="\t", index_col=0).loc['count', ['Blocks', 'Chimeric Blocks']].sum()
+        orb.loc['missed blocks', :] = trueBlocks - orb.loc[[c['label'] for _, c in settings['contig_classes'].items() if c['class'] == 'good'], :].sum()
+        # select metrics for "recovery analysis"
+        orb = orb.loc[[c['label'] for _, c in settings['contig_classes'].items()], :]
+        # sort assembler by amount of good contigs
+        orb = orb[orb.loc[[c['label'] for _, c in settings['contig_classes'].items() if c['class'] == 'good']].sum().sort_values(ascending=True).index]
+        # use pretty label for assembler
+        orb = orb.rename(columns=settings['labels']['assemblers'])
+        # transform axis: rows=assembler, cols=metrics+metadata
+        orb = orb.T
+        orb['environment'] = environment
+        orb['recovery_rank'] = list(reversed(range(1, orb.shape[0] + 1)))
+        data.append(orb)
+    return pd.concat(data)
+    
 def plot_recovery(fp_orb_basedir, settings, num_columns:int=3):
     """Plots contig recovery.
     
@@ -50,7 +89,10 @@ def plot_recovery(fp_orb_basedir, settings, num_columns:int=3):
     -------
     plt figure of the multi-pabel plot.
     """
-    environments = get_environments(fp_orb_basedir)
+    # which environments to plot and in which order
+    environments = get_environments(fp_orb_basedir, settings)
+    # load data
+    data_recovery = getdata_recovery(fp_orb_basedir, settings)
     
     fig, axes = plt.subplots(
         int(np.ceil(len(environments) / num_columns)), 
@@ -58,25 +100,12 @@ def plot_recovery(fp_orb_basedir, settings, num_columns:int=3):
         gridspec_kw={"wspace": 0.31, "hspace": 0.3})
     
     for i, environment in enumerate(environments):
-        # load orb data
-        orb = pd.read_csv(join(fp_orb_basedir, environment, 'mergedresults', '%s_all_scores.tsv' % environment), sep="\t", index_col=0)
-        # rename metrics to names used in publication
-        orb = orb.rename(index={k: c['label'] for k, c in settings['contig_classes'].items()})
-        # add information about missed blocks
-        trueBlocks = pd.read_csv(join(fp_orb_basedir, environment, 'mergedataframessummaries', '%s_all_contigs.tsv' % environment), sep="\t", index_col=0).loc['count', ['Blocks', 'Chimeric Blocks']].sum()
-        orb.loc['missed blocks', :] = trueBlocks - orb.loc[[c['label'] for _, c in settings['contig_classes'].items() if c['class'] == 'good'], :].sum()
-        # select metrics for "recovery analysis"
-        orb = orb.loc[[c['label'] for _, c in settings['contig_classes'].items()], :]
-        # sort assembler by amount of good contigs
-        orb = orb[orb.loc[[c['label'] for _, c in settings['contig_classes'].items() if c['class'] == 'good']].sum().sort_values(ascending=True).index]
-        # use pretty label for assembler
-        orb = orb.rename(columns=settings['labels']['assemblers'])
-    
-        # drawing
+        orb = data_recovery[data_recovery['environment'] == environment]
+
         # bad contigs
         ax_bad = axes[i // num_columns, (i % num_columns) * 2]
         ax_bad.invert_xaxis()
-        orb.loc[[c['label'] for _, c in settings['contig_classes'].items() if c['class'] == 'bad']].T.plot(kind='barh', stacked=True, ax=ax_bad, color={c['label']: c['color'] for _, c in settings['contig_classes'].items()})
+        orb.loc[:, [c['label'] for _, c in settings['contig_classes'].items() if c['class'] == 'bad']].plot(kind='barh', stacked=True, ax=ax_bad, color={c['label']: c['color'] for _, c in settings['contig_classes'].items()})
         ax_bad.set_xlabel("number contigs")
         ax_top_bad = ax_bad.twiny()
         ax_top_bad.xaxis.set_label_position('top')
@@ -84,10 +113,10 @@ def plot_recovery(fp_orb_basedir, settings, num_columns:int=3):
         ax_top_bad.set_xlabel("bad: artifacts")
         ax_bad.xaxis.set_label_coords(1, -0.08)
         ax_bad.text(-0.5, 1.05, chr(97+i), transform=ax_bad.transAxes, fontsize=16, fontweight='bold',)
-    
+
         # good contigs
         ax_good = axes[i // num_columns, (i % num_columns) * 2  + 1]
-        orb.loc[[c['label'] for _, c in settings['contig_classes'].items() if c['class'] != 'bad']].T.plot(kind='barh', stacked=True, ax=ax_good, color={c['label']: c['color'] for _, c in settings['contig_classes'].items()})
+        orb.loc[:, [c['label'] for _, c in settings['contig_classes'].items() if c['class'] != 'bad']].plot(kind='barh', stacked=True, ax=ax_good, color={c['label']: c['color'] for _, c in settings['contig_classes'].items()})
         ax_good.set_yticks([])
         ax_good.set_xlabel("good: recovered")
         ax_good.xaxis.set_label_position('top')
@@ -111,6 +140,7 @@ def plot_recovery(fp_orb_basedir, settings, num_columns:int=3):
         ax_bad.legend().remove()
 
     return fig
+
 
 # # above function should be called as following:
 # # 1) load plotting settings
